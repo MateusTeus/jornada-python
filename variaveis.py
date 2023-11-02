37d4ddadbf8307e02efffa7c6b679a38cbdda57b
Mateus Henrique <mateusteccruz@gmail.com>
2023-11-02 16:59:23 -0300

    Primeiro commit

--- variaveis.py
+++ variaveis.py
@@ -18,3 +18,42 @@ a = str(3)    # x will be '3'
 b = int(3)    # y will be 3
 c = float(3)  # z will be 3.0
 
+#consigo atribuit varios valores
+
+f, g, h , = 'Banana','Maça',"Pera"
+print(f,g,h)    #f = Banana   g = Maça  h = Pera
+f = g = h  = 'Banana'
+print(f,g,h)    # f recebe g que recebe h  = 'Banana
+
+#descompactando
+
+
+frutas = ['Maça','Pera','Uva']
+
+a, b, c = frutas
+print(a)  #Maça
+print(b)  #Pera
+print(c)  #Uva
+
+
+#variaveis globais
+
+x = "incrivel"
+
+def myfunc():
+  x = "fantastico"
+  print("Python é " + x)     #fantastico
+
+myfunc()
+
+print("Python é " + x)     #incrivel
+
+#palavra chave global torna global
+
+def outraFunc():
+    global x
+    x = "Palavra"  
+    outraFunc()     
+  
+print(x) 
+
