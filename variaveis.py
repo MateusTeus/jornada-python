d35425ca0076e7a05f94b577dad34c91ccb17c21
Mateus Henrique <mateusteccruz@gmail.com>
2023-11-02 16:31:03 -0300

    Variaveis em python

---
+++ variaveis.py
@@ -0,0 +1,20 @@
+nome = "Mateus"
+sobrenome = "Henrique"
+idade = 18
+altura = 1.87
+estudante = True
+print(nome + " " + sobrenome)
+print(type(nome))       #str
+print(type(idade))      #int
+print(type(altura))     #float
+print(type(estudante))  #boll
+
+x = 4       # x é int
+print(type(x))
+x = "Sally" # x agora é string
+print(type(x))
+
+a = str(3)    # x will be '3'
+b = int(3)    # y will be 3
+c = float(3)  # z will be 3.0
+
